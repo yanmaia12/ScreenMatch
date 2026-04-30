75a4a006d96a5fe9164c6f0996ff45b5a0179d10
Yan Maia <yandantasmaia12@gmail.com>
2026-04-30 16:20:46 +0100

    test: teste unitário para o método obterTop5Series da classe SerieService, existindo e não existindo

--- screenMatch/src/test/java/com/yanmaia12/screenMatch/Service/SerieServiceTest.java
+++ screenMatch/src/test/java/com/yanmaia12/screenMatch/Service/SerieServiceTest.java
@@ -66,4 +66,34 @@ public class SerieServiceTest {
 
         Mockito.verify(serieRepository, Mockito.times(1)).findAll();
     }
+
+    @Test
+    void deveRetornarTop5SeriesQuandoExistirem(){
+        Serie serie1 = new Serie();
+        Serie serie2 = new Serie();
+        Serie serie3 = new Serie();
+        Serie serie4 = new Serie();
+        Serie serie5 = new Serie();
+
+        List<Serie> listaSerieTeste = List.of(serie1, serie2, serie3, serie4, serie5);
+
+        Mockito.when(serieRepository.findTop5ByOrderByAvaliacaoDesc()).thenReturn(listaSerieTeste);
+
+        List<SerieDTO> listaSerie = serieService.obterTop5Series();
+
+        Assertions.assertEquals(5, listaSerie.size());
+        Mockito.verify(serieRepository, Mockito.times(1)).findTop5ByOrderByAvaliacaoDesc();
+    }
+
+    @Test
+    void deveRetornarNullQuandoChamaTop5SeriesQueNaoExistem(){
+        List<Serie> listaSerieTeste = new ArrayList<>();
+
+        Mockito.when(serieRepository.findTop5ByOrderByAvaliacaoDesc()).thenReturn(listaSerieTeste);
+
+        List<SerieDTO> listaSerie = serieService.obterTop5Series();
+
+        Assertions.assertEquals(0, listaSerie.size());
+        Mockito.verify(serieRepository, Mockito.times(1)).findTop5ByOrderByAvaliacaoDesc();
+    }
 }
